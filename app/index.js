3ca23d9b64356644b4d8e6746707d4912b47ea79
Kevin <mr.kev@me.com>
2014-06-10 13:04:10 -0500

    Fix problems. Clean up.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,45 @@
 'use strict';
-var util = require('util');
-var path = require('path');
+//var util = require('util');
+//var path = require('path');
 var yosay = require('yosay');
-var chalk = require('chalk');
+//var chalk = require('chalk');
 var yeoman = require('yeoman-generator');
+
+
+/*
+ *
+ * Helper functions
+ * 
+ */
+
+
+/**
+ * Returns path to directory, excluding file, if
+ * any.
+ * @param  {String} path [description]
+ * @return {String}      [description]
+ */
+function dirFor (path) {
+  if (path.indexOf('.') === -1) { return path; } 
+  return path.substring(0, path.lastIndexOf('/'));
+}
+
+/**
+ * Generates an algebraic project name
+ * @return {String} Mathematical title
+ */
+function genProjName () {
+  var adj = ['awesome', 'marvelous', 'fantastic', 'mathematical', 'algebraic'];
+  var sub = ['project', 'madness', 'goodness', 'awesomeness', 'coolness', 'typescriptness'];
+  return adj[Math.floor(Math.random()*adj.length)] + '-typescript-' + sub[Math.floor(Math.random()*sub.length)];
+}
+
+
+/*
+ * 
+ * The deal
+ * 
+ */
 
 
 var TypescriptGenerator = yeoman.generators.Base.extend({
@@ -22,9 +58,9 @@ var TypescriptGenerator = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay('Let\'s make some awesome typescript project!'));
-    this.log('I will include JSHint and Editorconfig by default.')
+    this.log('I will include JSHint and Editorconfig by default.');
 
-    this.extfeat = {"Throw in some Bower too" : "bower"};
+    this.extfeat = {'Throw in some Bower too' : 'bower'};
 
     var prompts = [
     {
@@ -32,28 +68,28 @@ var TypescriptGenerator = yeoman.generators.Base.extend({
       name    : 'projectName',
       message : 'First off, how would you like to name this project?',
       default : genProjName()
-    }
-    
-    ,{
+    },
+
+    {
       type    : 'list',
       name    : 'moduleType',
       message : 'Would you like to use AMD or CommonJS?',
-      choices : [ "CommonJS", "AMD" ],
-      default : "CommonJS"
-    }
+      choices : [ 'CommonJS', 'AMD' ],
+      default : 'CommonJS'
+    },
 
-    ,{
+    {
       type    : 'input',
       name    : 'tsDest',
       message : 'Where should it be compiled to?',
-      default : "app/build"
-    }
-    
-    ,{
+      default : 'app/build'
+    },
+
+    {
       type    : 'input',
       name    : 'tsSrc',
       message : 'Where should your typescript go?',
-      default : "app/src"
+      default : 'app/src'
     }
 
     ];
@@ -63,11 +99,11 @@ var TypescriptGenerator = yeoman.generators.Base.extend({
       this.tsSrc       = props.tsSrc;
       this.tsDest      = props.tsDest;
       this.genMaps     = props.genMaps;
-      this.projectName = props.projectName.replace(" ", "-");
+      this.projectName = props.projectName.replace(' ', '-');
       this.extra       = props.extra;
       this.genMaps     = props.genMaps;
 
-      if (props.extra != undefined) {
+      if (props.extra !== undefined) {
         for (var i = 0; i < props.extra.length; i++) {
           switch (this.extfeat[i]) {
             case 'bower'    : {this.bower     = true; } break;
@@ -82,7 +118,7 @@ var TypescriptGenerator = yeoman.generators.Base.extend({
       this.username    = this.user.git.username;
       this.email       = this.user.git.email;
 
-      this.defaultMain = dirFor(this.tsDest) + "/index.js"
+      this.defaultMain = dirFor(this.tsDest) + '/index.js';
 
       done();
     }.bind(this));
@@ -99,16 +135,16 @@ var TypescriptGenerator = yeoman.generators.Base.extend({
     this.template('_gulpfile.js', 'gulpfile.js');
 
     // Tests
-    this.mkdir("test");
-    this.template("_test-greeting.js", 'test/test-greeting.js');
-    this.template("_test-load.js", 'test/test-load.js');
+    this.mkdir('test');
+    this.template('_test-greeting.js', 'test/test-greeting.js');
+    this.template('_test-load.js', 'test/test-load.js');
 
     // Readme
     this.template('_README.md', 'README.md');
   },
 
   projectfiles: function () {
-    if (this.bower) this.template('_bower.json', 'bower.json');
+    if (this.bower) { this.template('_bower.json', 'bower.json'); }
     this.copy('editorconfig', '.editorconfig');
     this.copy('jshintrc'    , '.jshintrc');
   }
@@ -118,23 +154,3 @@ var TypescriptGenerator = yeoman.generators.Base.extend({
 });
 
 module.exports = TypescriptGenerator;
-
-
-// Helper functions
-
-/**
- * Returns path to directory, excluding file, if
- * any.
- * @param  {[type]} path [description]
- * @return {[type]}      [description]
- */
-function dirFor (path) {
-  if (path.indexOf(".") == -1) return path;
-  return path.substring(0, path.lastIndexOf("/"));
-}
-
-function genProjName () {
-  var adj = ['awesome', 'marvelous', 'fantastic', 'mathematical', 'algebraic'];
-  var sub = ['project', 'madness', 'goodness', 'awesomeness', 'coolness', 'typescriptness']
-  return adj[Math.floor(Math.random()*adj.length)] + '-typescript-' + sub[Math.floor(Math.random()*sub.length)];
-}
